0f317faa6bceee3115d3b28153bc10ec29be1df6
iRing21 <ian.ring21@gmail.com>
2021-08-06 13:48:48 -0400

    changed tabs, started adding descriptions, rearranged some of the layout and also adjusted color scheme

--- piAudioSite/scripts.js
+++ piAudioSite/scripts.js
@@ -1,41 +1,40 @@
 
 document.getElementById("compButt").addEventListener("click", function() {
     document.getElementById("compositionDesc").style.display = "inline";
-    document.getElementById("editingDesc").style.display = "none";
     document.getElementById("mixingDesc").style.display = "none";
-    document.getElementById("masteringDesc").style.display = "none";
-    document.getElementById("voiceDesc").style.display = "none";
-
+    document.getElementById("advertisingDesc").style.display = "none";
+    document.getElementById("contactDesc").style.display = "none";
+    document.getElementById("bioDesc").style.display = "none";
 });
 
-document.getElementById("editingButt").addEventListener("click", function() {
-    document.getElementById("editingDesc").style.display = "inline";
+document.getElementById("advertisingButt").addEventListener("click", function() {
     document.getElementById("compositionDesc").style.display = "none";
     document.getElementById("mixingDesc").style.display = "none";
-    document.getElementById("masteringDesc").style.display = "none";
-    document.getElementById("voiceDesc").style.display = "none";
+    document.getElementById("advertisingDesc").style.display = "inline";
+    document.getElementById("contactDesc").style.display = "none";
+    document.getElementById("bioDesc").style.display = "none";
 });
 
 document.getElementById("mixingButt").addEventListener("click", function() {
+    document.getElementById("compositionDesc").style.display = "none";
     document.getElementById("mixingDesc").style.display = "inline";
-    document.getElementById("compositionDesc").style.display = "none";
-    document.getElementById("editingDesc").style.display = "none";
-    document.getElementById("masteringDesc").style.display = "none";
-    document.getElementById("voiceDesc").style.display = "none";
+    document.getElementById("advertisingDesc").style.display = "none";
+    document.getElementById("contactDesc").style.display = "none";
+    document.getElementById("bioDesc").style.display = "none";
 });
 
-document.getElementById("masteringButt").addEventListener("click", function() {
-    document.getElementById("masteringDesc").style.display = "inline";
+document.getElementById("contactButt").addEventListener("click", function() {
     document.getElementById("compositionDesc").style.display = "none";
-    document.getElementById("editingDesc").style.display = "none";
     document.getElementById("mixingDesc").style.display = "none";
-    document.getElementById("voiceDesc").style.display = "none";
+    document.getElementById("advertisingDesc").style.display = "none";
+    document.getElementById("contactDesc").style.display = "inline";
+    document.getElementById("bioDesc").style.display = "none";
 });
 
-document.getElementById("voiceButt").addEventListener("click", function() {
-    document.getElementById("voiceDesc").style.display = "inline";
+document.getElementById("bioButt").addEventListener("click", function() {
     document.getElementById("compositionDesc").style.display = "none";
-    document.getElementById("editingDesc").style.display = "none";
     document.getElementById("mixingDesc").style.display = "none";
-    document.getElementById("masteringDesc").style.display = "none";
+    document.getElementById("advertisingDesc").style.display = "none";
+    document.getElementById("contactDesc").style.display = "none";
+    document.getElementById("bioDesc").style.display = "inline";
 });
